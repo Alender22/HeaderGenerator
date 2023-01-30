586b906ae12ecf86d92e91ea1a5671a302d12fe9
Lucas <lucas@ethington.de>
2023-01-30 09:10:03 -0600

    removing color tests, they moved to another repo

--- main.cpp
+++ main.cpp
@@ -10,25 +10,7 @@ int doFileFormatting(int argC, char** args);
 
 int main(int argC, char** args)
 {
-    /*string origin = "testchekingtesthow welltestthis is working nowtest";
-    string splt = "test";
-
-    vector<string> tokens;
-    tokens.push_back(replaceSubStringWithString(origin, splt, "Function"));
-
-    for(int i = 0; i < tokens.size(); i++)
-    {
-        cout << "|" << tokens[i] << "|" << " length: " << tokens[i].length() << endl;
-    }*/
-
-    //cout << setBackgroundColor("  ", 255, 0, 0) << getTermWidth() << "|" << getTermHeight() << endl;
-
-    for(int r = 0; r < 255; r++)
-        for(int g = 0; g < 255; g++)
-            for(int b = 0; b < 255; b++)
-                cout << setBackgroundColor(" ", r, g, b);
-
-    //return doFileFormatting(argC, args);
+    return doFileFormatting(argC, args);
 }
 
 int doFileFormatting(int argC, char** args)
--- terminalTools.cpp
+++ terminalTools.cpp
@@ -3,33 +3,6 @@
 
 using namespace std;
 
-void placeCursorAt(int i, int j)
-{
-    cout << "\033[" + to_string(i) + ";" + to_string(j) + "f";
-}
-
-void clearTerminal()
-{
-    struct winsize termSize;
-    ioctl(0, TIOCGWINSZ, & termSize);
-
-    for(int i = 0; i <= termSize.ws_row; i++)
-    {
-        placeCursorAt(i, 1);
-        cout << "\033[2K";
-    }
-
-    placeCursorAt(1, 1);
-}
-
-void makeSpace(int lines)
-{
-    for(int i = 0; i < lines; i++)
-    {
-        cout << endl;
-    }
-}
-
 int getTermWidth()
 {
     struct winsize termSize;
@@ -44,4 +17,28 @@ int getTermHeight()
     ioctl(0, TIOCGWINSZ, & termSize);
 
     return termSize.ws_row;
+}
+
+void placeCursorAt(int i, int j)
+{
+    cout << "\033[" + to_string(i) + ";" + to_string(j) + "f";
+}
+
+void clearTerminal()
+{
+    for(int i = 0; i <= getTermHeight(); i++)
+    {
+        placeCursorAt(i, 1);
+        cout << "\033[2K";
+    }
+
+    placeCursorAt(1, 1);
+}
+
+void makeSpace(int lines)
+{
+    for(int i = 0; i < lines; i++)
+    {
+        cout << endl;
+    }
 }
